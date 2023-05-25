86e79fc33b35b19b969eb672ebf542827da23137
GhostPowerShell <GhostPowerShell>
2023-05-25 11:58:36 +0600

    Added first version front-end

---
+++ frontend/static/js/script.js
@@ -0,0 +1,32 @@
+
+    const socket = new WebSocket("ws://localhost:8080/bust"); // Замените на актуальный URL вашего WebSocket сервера
+
+    socket.addEventListener("open", (event) => {
+    console.log("Connected to WebSocket server");
+});
+
+    socket.addEventListener("message", (event) => {
+    const message = event.data;
+    addMessageToDOM(message);
+});
+
+    socket.addEventListener("close", (event) => {
+    console.log("Disconnected from WebSocket server");
+});
+
+    function sendMessage() {
+    const inputElement = document.getElementById("inputMessage");
+    const message = inputElement.value;
+    socket.send(JSON.stringify({ url: message }));
+    inputElement.value = "";
+}
+
+    function addMessageToDOM(message) {
+    const messagesElement = document.getElementById("messages");
+    const messageElement = document.createElement("div");
+    messageElement.textContent = message;
+    messagesElement.appendChild(messageElement);
+}
+    function stopWebSocket() {
+        socket.close();
+    }
